56181f1d149c487fbc9ee1ac0df7bdb2b114d20f
Abdul-Akeem Kako - STUDENT <l00192762@atu.ie>
2026-01-28 13:54:14 +0000

    Added HelloATU.java

---
+++ HelloATU.java
@@ -0,0 +1,7 @@
+public class HelloATU {
+    public static void main(String[] args) {
+
+        // Prints "Hello, ATU from Abdul" as output
+        System.out.println("Hello, ATU from Abdul");
+    }
+}
